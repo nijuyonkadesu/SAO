f2aea460271cda770150f6cc1307c4972f944ed6
nijuyonkadesu <24ksriramv@gmail.com>
2023-06-12 10:37:38 +0530

    rename to CarouselFragment

--- app/src/main/java/one/njk/sao/CarouselFragment.kt
+++ app/src/main/java/one/njk/sao/CarouselFragment.kt
@@ -12,14 +12,14 @@ import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
-import one.njk.sao.databinding.FragmentFirstBinding
+import one.njk.sao.databinding.FragmentCarouselBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment(), MenuProvider {
+class CarouselFragment : Fragment(), MenuProvider {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentCarouselBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -33,7 +33,7 @@ class FirstFragment : Fragment(), MenuProvider {
         // Menu Provider method: https://stackoverflow.com/questions/71917856/sethasoptionsmenuboolean-unit-is-deprecated-deprecated-in-java
         val menuHost: MenuHost = requireActivity()
         menuHost.addMenuProvider( this, viewLifecycleOwner, Lifecycle.State.RESUMED)
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentCarouselBinding.inflate(inflater, container, false)
 
         _binding!!.bottomAppBar.setOnMenuItemClickListener {
             when(it.itemId){
