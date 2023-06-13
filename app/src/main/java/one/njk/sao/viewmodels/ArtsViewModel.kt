e7eab4763cd97be662be800db086ffe70201058b
nijuyonkadesu <24ksriramv@gmail.com>
2023-06-13 19:05:19 +0530

    consume retrofit results with viewmodel

--- app/src/main/java/one/njk/sao/viewmodels/ArtsViewModel.kt
+++ app/src/main/java/one/njk/sao/viewmodels/ArtsViewModel.kt
@@ -1,18 +1,29 @@
 package one.njk.sao.viewmodels
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.flow
 import one.njk.sao.models.Waifu
+import one.njk.sao.network.WaifuApiService
+import javax.inject.Inject
 
-class ArtsViewModel: ViewModel() {
-    // TODO: 1. Build a retrofit client with different sfw point
-    // TODO: 2. Add Small text in carosuel (and try to make it vertical ?)
-    // TODO: 3. !! HOW TO TRACK WHICH IMAGE I'M IN !!!
+@HiltViewModel
+class ArtsViewModel @Inject constructor(
+    private val waifuApiService: WaifuApiService
+) : ViewModel() {
+    // TODO: 1. Paging (auto fetching)
+    // TODO: 2 Switch Sources when category and type changes
+    // TODO: 2. !! HOW TO TRACK WHICH IMAGE I'M IN !!!
 
-    val waifuList = flow { emit(listOf(
-        Waifu("1", "https://i.waifu.pics/Tj6Wzwo.png"),
-        Waifu("2", "https://i.waifu.pics/ZBCHHyT.jpg"),
-        Waifu("3", "https://i.waifu.pics/04afe1y.jpg"),
-        Waifu("4", "https://i.waifu.pics/qnlMuuF.gif")
-    )) }
+    val waifuList = flow {
+        val waifus = mutableListOf<Waifu>()
+
+        waifuApiService.getWaifus(
+            type = "sfw",
+            category = "waifu",
+            exclude = listOf("nsfw")
+        ).files.map { waifus.add(Waifu(it)) }
+
+        emit(waifus)
+    }
 }
