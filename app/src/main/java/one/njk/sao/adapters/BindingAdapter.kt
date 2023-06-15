76e948f045fc2555c6e71dd58add72b043c8f307
nijuyonkadesu <24ksriramv@gmail.com>
2023-06-15 17:22:19 +0530

    customizing ImageLoader through factory class, remove redundant id

--- app/src/main/java/one/njk/sao/adapters/BindingAdapter.kt
+++ app/src/main/java/one/njk/sao/adapters/BindingAdapter.kt
@@ -3,17 +3,14 @@ package one.njk.sao.adapters
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
-import coil.ImageLoader
-import coil.decode.ImageDecoderDecoder
+import coil.imageLoader
 import coil.request.ImageRequest
 
 @BindingAdapter("imageUrl")
 fun bindImage(imgView: ImageView, imgUrl: String?) {
-    // Custom ImageLoader with GIF support
-    val imageLoader = ImageLoader.Builder(imgView.context)
-        .components {
-            add(ImageDecoderDecoder.Factory())
-        }.build()
+
+    // Custom ImageLoader with GIF support obtained from factory class
+    val imageLoader = imgView.context.imageLoader
 
     if(!imgUrl.isNullOrEmpty()){
         val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
