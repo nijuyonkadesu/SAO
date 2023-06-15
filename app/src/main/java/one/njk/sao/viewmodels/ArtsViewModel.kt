d91aa90a173111f6a59c26a695afbcbdf4fe9066
nijuyonkadesu <24ksriramv@gmail.com>
2023-06-15 18:16:11 +0530

    use on scroll listener to refresh the list

--- app/src/main/java/one/njk/sao/viewmodels/ArtsViewModel.kt
+++ app/src/main/java/one/njk/sao/viewmodels/ArtsViewModel.kt
@@ -11,18 +11,20 @@ import javax.inject.Inject
 class ArtsViewModel @Inject constructor(
     private val waifuApiService: WaifuApiService
 ) : ViewModel() {
-    // TODO: 1. Paging (auto fetching)
+    // TODO: 0 handle network disconnect crash
+    // TODO: 1. !! HOW TO TRACK WHICH IMAGE I'M IN !!!
     // TODO: 2 Switch Sources when category and type changes
-    // TODO: 2. !! HOW TO TRACK WHICH IMAGE I'M IN !!!
+    // TODO: 3 Survive process deaths (using savedstate handle & lifecycle methods)
+    private val waifus = mutableListOf<Waifu>()
 
     val waifuList = flow {
-        val waifus = mutableListOf<Waifu>()
-
         waifuApiService.getWaifus(
             type = "sfw",
             category = "waifu",
             exclude = listOf("nsfw")
         ).files.map { waifus.add(Waifu(it)) }
+        // technically, the files you've seen already can be send here as exclude,
+        // so that you'll never see those files again... but... ah, it grows too big soon
 
         emit(waifus)
     }
