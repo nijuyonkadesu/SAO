5d0766df7b1f810443a74d4c471f31758726cd91
nijuyonkadesu <24ksriramv@gmail.com>
2023-07-01 21:11:13 +0530

    Share Intent (2/2) image share

--- app/src/main/java/one/njk/sao/adapters/CarouselAdapter.kt
+++ app/src/main/java/one/njk/sao/adapters/CarouselAdapter.kt
@@ -1,9 +1,10 @@
 package one.njk.sao.adapters
 
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
-import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
+import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
@@ -12,11 +13,13 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.ImageLoader
-import coil.request.ImageRequest
+import coil.annotation.ExperimentalCoilApi
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import one.njk.sao.databinding.CarouselViewWaifuBinding
 import one.njk.sao.models.Waifu
+import java.io.File
+import java.io.OutputStream
 
 class CarouselAdapter(
     val imageLoader: ImageLoader,
@@ -41,9 +44,9 @@ class CarouselAdapter(
         val lifecycleScope: CoroutineScope
     ):
         RecyclerView.ViewHolder(binding.root) {
+            @OptIn(ExperimentalCoilApi::class)
             fun bind(waifu: Waifu){
                 binding.apply {
-//                    lifecycleOwner = viewLifecycleOwner
                     this.waifu = waifu
                     this.imageLoader = imageLoaderHilt
                     executePendingBindings()
@@ -51,29 +54,66 @@ class CarouselAdapter(
                 binding.carouselItemContainer.setOnClickListener {
                     lifecycleScope.launch {
                         Log.d("url", waifu.url)
-                        val request = ImageRequest.Builder(context)
-                            .data(waifu.url)
-                            .build()
-                        val drawable = imageLoaderHilt.execute(request).drawable
-                        val bitmap = (drawable as? BitmapDrawable)?.bitmap
 
-                        val path = MediaStore.Images.Media.insertImage(
-                            context.contentResolver,
-                            bitmap,
-                            "SAO:",
-                            null
-                        )
+                        imageLoaderHilt.diskCache!!.openSnapshot(waifu.url)?.let {
+                            val file = it.data.toFile()
+                            Log.d("url", file.length().toString())
+                            it.close()
 
-                        val shareIntent = Intent(Intent.ACTION_SEND).apply {
-                            type = "image/*"
-                            val uri = Uri.parse(path)
-                            putExtra(Intent.EXTRA_STREAM, uri)
-                            putExtra(Intent.EXTRA_TEXT, "sus")
+                            val shareIntent = Intent(Intent.ACTION_SEND).apply {
+                                type = "image/*"
+                                val uri = uriFromFile(file, waifu.url)
+                                putExtra(Intent.EXTRA_STREAM, uri)
+                            }
+                            context.startActivity(Intent.createChooser(shareIntent, "Share Image"))
                         }
-                        context.startActivity(Intent.createChooser(shareIntent, "Share Image"))
                     }
                 }
             }
+
+        fun uriFromFile(file: File, url: String): Uri {
+            val filename = getFilename(url)
+            val ext = getFileExtension(url)
+
+            var imageUri: Uri?
+            var fos: OutputStream?
+
+            val contentValues = ContentValues().apply {
+                val mimeType = when (ext) {
+                    "jpeg" -> "image/jpeg"
+                    "jpg" -> "image/jpeg"
+                    "png" -> "image/png"
+                    "gif" -> "image/gif"
+                    else -> "image/jpeg"
+                }
+
+                put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+                put(MediaStore.MediaColumns.MIME_TYPE, mimeType)
+                put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                put(MediaStore.Video.Media.IS_PENDING, 1)
+            }
+            context.contentResolver.also { resolver ->
+                imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+                fos = imageUri?.let { resolver.openOutputStream(it) }
+            }
+
+            // TODO: Use FileProvider and avoid saving file when sharing
+            fos?.write(file.readBytes())
+
+            contentValues.clear()
+            contentValues.put(MediaStore.Video.Media.IS_PENDING, 0)
+            context.contentResolver.update(imageUri!!, contentValues, null, null)
+
+            return imageUri!!
+        }
+        fun getFilename(url: String): String {
+            val endpoint = url.split('.')
+            return endpoint[endpoint.lastIndex - 1].split('/').last()
+        }
+
+        fun getFileExtension(url: String): String {
+            return url.split('.').last()
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WaifuViewHolder {
