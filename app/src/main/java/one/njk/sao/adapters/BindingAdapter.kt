837bf3940107d4d42c74163a0bad4175185fb093
nijuyonkadesu <24ksriramv@gmail.com>
2023-07-10 11:21:33 +0530

    add image placeholder

--- app/src/main/java/one/njk/sao/adapters/BindingAdapter.kt
+++ app/src/main/java/one/njk/sao/adapters/BindingAdapter.kt
@@ -5,6 +5,7 @@ import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import coil.ImageLoader
 import coil.request.ImageRequest
+import one.njk.sao.R
 
 @BindingAdapter("imageUrl", "imageLoader")
 fun bindImage(imgView: ImageView, imgUrl: String?, imageLoader: ImageLoader) {
@@ -12,10 +13,12 @@ fun bindImage(imgView: ImageView, imgUrl: String?, imageLoader: ImageLoader) {
     if(!imgUrl.isNullOrEmpty()){
         val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
         // pass a request to imageloader
+        // https://t.me/addstickers/LINE_White_Cat_Girl - awesome Sticker Pack By LINE!
         imageLoader.enqueue(
             ImageRequest.Builder(imgView.context)
                 .data(imgUri)
                 .target(imgView)
+                .placeholder(R.drawable.eager)
                 .build()
         )
         // TODO: Have placeholders and optional transformers
